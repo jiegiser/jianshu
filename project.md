55457cc9ae1563dd5c151b547b913f190dd68df7
jiegiser <cjieouba@outlook.com>
2020-03-02 08:37:17 +0800

    Init: 项目初始化

---
+++ project.md
@@ -0,0 +1,10 @@
+<!--
+ * @Descripttion: 
+ * @Author: jiegiser
+ * @Date: 2020-03-02 08:00:59
+ * @LastEditors: jiegiser
+ * @LastEditTime: 2020-03-02 08:01:55
+ -->
+## styled-components
+使用styled-components去管理项目中的样式。
+首先需要进行安装：yarn add styled-components。
---
+++ src/App.js
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion: 
+ * @Author: jiegiser
+ * @Date: 2020-03-02 07:49:57
+ * @LastEditors: jiegiser
+ * @LastEditTime: 2020-03-02 08:24:51
+ */
+import React, { Fragment } from 'react'
+import { Globalstyle } from './style'
+function App() {
+  return (
+    <Fragment>
+      <Globalstyle/>
+      <div className= 'red'>
+        hello
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
