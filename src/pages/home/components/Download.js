4c74c3c482a576a7bb67ed5811f87794c2dce710
jiegiser <cjieouba@outlook.com>
2020-03-06 07:47:48 +0800

    Feat: 增加下载app布局

---
+++ src/pages/home/components/Download.js
@@ -0,0 +1,24 @@
+/*
+ * @Descripttion: 
+ * @Author: jiegiser
+ * @Date: 2020-03-05 13:58:59
+ * @LastEditors: jiegiser
+ * @LastEditTime: 2020-03-05 14:29:29
+ */
+import React, { Component } from 'react'
+import {
+  DownloadWrapper,
+  DowmloadDesc
+} from '../style'
+const Download = () => {
+  return (
+    <DownloadWrapper>
+      <img alt="" src="http://cdn2.jianshu.io/assets/web/download-index-side-qrcode-cb13fc9106a478795f8d10f9f632fccf.png" />
+      <DowmloadDesc>
+        <div className="up">下载简书手机App</div>
+        <div className="down">随时随地发现和创作内容</div>
+      </DowmloadDesc>
+    </DownloadWrapper>
+  )
+}
+export default Download
